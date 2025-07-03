1998cdca24b365fd6ca08253816f6b447091b37e
kindliai <ts.techsolutionsgmbh@gmail.com>
2025-07-03 16:37:57 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const multer  = require('multer');
+const fs      = require('fs');
+const { exec }= require('child_process');
+
+const app = express();
+const upload = multer({ dest: 'uploads/' });
+
+app.post('/convert', upload.single('audio'), (req, res) => {
+  const inFile  = req.file.path;
+  const outFile = `${inFile}.mp3`;
+
+  exec(`ffmpeg -f s16le -ar 24000 -ac 1 -i ${inFile} -codec:a libmp3lame -qscale:a 4 ${outFile}`,
+    err => {
+      if (err) return res.status(500).send('ffmpeg error');
+      res.download(outFile, 'audio.mp3', () => {
+        fs.unlinkSync(inFile);
+        fs.unlinkSync(outFile);
+      });
+    });
+});
+
+app.listen(3000, () => console.log('Up on 3000'));
